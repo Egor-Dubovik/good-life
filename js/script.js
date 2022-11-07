4ad6871e1ea6357030dc467251b78d05e792dc81
Egor Dubovik <egorka54896@gmail.com>
2022-11-07 17:07:00 +0300

    feat: create page for top10 sport

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//arrow down------------------------------------------
+function scrollTo(event) {
+	window.scroll({
+		left: 0,
+		top: event.offsetTop - 0,
+		behavior: 'smooth'
+	})
+};
+const arrowDown = document.querySelector('.btn-down');
+const blockTraditions = document.querySelector('.aa');
+
+arrowDown.addEventListener('click', (e) => {
+	e.preventDefault();
+	scrollTo(blockTraditions);
+});
+//-----------------------------------------------------------------------
