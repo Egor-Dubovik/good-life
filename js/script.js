231065d40f8ad9309dbaee1a114cc761b80c6c04
Egor Dubovik <egorka54896@gmail.com>
2022-11-26 14:08:17 +0300

    feat: add page videos and top 5

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 //arrow down------------------------------------------
-function scrollTo(event) {
-	window.scroll({
-		left: 0,
-		top: event.offsetTop - 0,
-		behavior: 'smooth'
-	})
-};
-const arrowDown = document.querySelector('.btn-down');
-const blockTraditions = document.querySelector('.aa');
+// function scrollTo(event) {
+// 	window.scroll({
+// 		left: 0,
+// 		top: event.offsetTop - 0,
+// 		behavior: 'smooth'
+// 	})
+// };
+// const arrowDown = document.querySelector('.btn-down');
+// const blockTraditions = document.querySelector('.aa');
 
-arrowDown.addEventListener('click', (e) => {
-	e.preventDefault();
-	scrollTo(blockTraditions);
-});
+// arrowDown.addEventListener('click', (e) => {
+// 	e.preventDefault();
+// 	scrollTo(blockTraditions);
+// });
 //-----------------------------------------------------------------------
+
+
+import {switchMenu} from "./modules/switchMenu.js";
+
+switchMenu();
